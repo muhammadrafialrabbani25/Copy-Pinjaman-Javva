1386ef4db255141eb2ff78e8c1c86be6342118c6
kemalmelvinibrahim66 <kemal.melvin.tif425@polban.ac.id>
2026-05-06 12:20:04 +0700

    membuat skeleton file cancel

---
+++ pinjamanjavva/src/main/java/com/p2plending/application/borrower/dto/CancelLoanCommand.java
@@ -0,0 +1,12 @@
+package com.p2plending.application.borrower.dto;
+
+/**
+ * Command DTO: Cancel Loan Request
+ * 
+ * Input from borrower to cancel an active loan
+ */
+public class CancelLoanCommand {
+
+    // Constructors
+    
+}
---
+++ pinjamanjavva/src/main/java/com/p2plending/application/borrower/usecase/CancelLoanUseCase.java
@@ -0,0 +1,6 @@
+package com.p2plending.application.borrower.usecase;
+
+import com.p2plending.application.borrower.dto.CancelLoanCommand;
+import com.p2plending.domain.borrower.entity.Borrower;
+import com.p2plending.domain.borrower.entity.LoanApplication;
+import com.p2plending.domain.borrower.service.LoanCancellationService;
