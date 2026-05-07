4e214080f2cb184e3b7343ea7746ae93aa78d97e
kemalmelvinibrahim66 <kemal.melvin.tif425@polban.ac.id>
2026-05-07 21:43:52 +0700

    membuat dan menyelesaikan file DomainEventPublisher.java

--- pinjamanjavva/src/main/java/com/p2plending/domain/shared/DomainEventPublisher.java
+++ pinjamanjavva/src/main/java/com/p2plending/domain/shared/DomainEventPublisher.java
@@ -1,4 +1,13 @@
 package com.p2plending.domain.shared;
 
+import java.util.EventListener;
+import java.util.List;
+
 public interface DomainEventPublisher {
+    void subscribe(String eventType, EventListener listener);
+    /* Subscribe listener untuk menerima event type tertentu */
+    void publish(Object event);
+    /* Publish event ke semua listeners yang subscribe */
+    List <EventListener> getListeners(String eventType);
+    /* Get semua listeners untuk event type tertentu */
 }
